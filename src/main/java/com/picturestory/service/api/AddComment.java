9d813af8c2c20040270af48aa182b8a96f7c61ec
Prasanna Bankuru <bankuru.prasanna@inmobi.com>
2016-04-30 23:02:06 +0530

    update endpoints

--- src/main/java/com/picturestory/service/api/AddComment.java
+++ src/main/java/com/picturestory/service/api/AddComment.java
@@ -1,13 +1,14 @@
 package com.picturestory.service.api;
 
-
-import com.picturestory.service.Constants;
+import com.picturestory.service.Constants ;
 import com.picturestory.service.database.dao.IContentDetailsDao;
 import com.picturestory.service.database.dao.IContentUserCommentDao;
 import com.picturestory.service.database.dao.IUserDetailsDao;
 import com.picturestory.service.pojo.ContentUserCommentAssociation;
-import com.picturestory.service.request.DeleteCommentOnContentRequest;
+import com.picturestory.service.request.AddCommentOnContentRequest;
 import com.picturestory.service.response.ResponseBuilder;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -15,57 +16,69 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-
+import java.util.Random;
 /**
  * Created by aasha.medhi on 9/25/15.
  */
-@Path("/deleteCommentOnContent")
+@Path("/addComment")
 @Produces("application/json")
 @Consumes("application/json")
-public class DeleteCommentContent {
+public class AddComment {
     private final IUserDetailsDao mUserDetailsDao;
     private final IContentUserCommentDao mContentUserCommentDao;
     private final IContentDetailsDao mContentDetailsDao;
+
     @Inject
-    public DeleteCommentContent(IUserDetailsDao userDetailsDao, IContentUserCommentDao contentUserCommentDao, IContentDetailsDao contentDetailsDao) {
+    public AddComment(IUserDetailsDao userDetailsDao, IContentUserCommentDao contentUserCommentDao, IContentDetailsDao contentDetailsDao) {
         mUserDetailsDao = userDetailsDao;
         mContentUserCommentDao = contentUserCommentDao;
         mContentDetailsDao = contentDetailsDao;
     }
 
     @POST
-    public Response deleteCommentOnContent(DeleteCommentOnContentRequest deleteCommentOnContentRequest) {
+    public Response addCommentOnContent(AddCommentOnContentRequest commentContentRequest) {
         try {
-            if(deleteCommentOnContentRequest == null){
+
+            if (commentContentRequest == null) {
                 return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, Constants.INVALID_REQUEST);
             }
-            if(!deleteCommentOnContentRequest.isValid())
-            {
-                return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, deleteCommentOnContentRequest.errorMessage());
+            if (!commentContentRequest.isValid()) {
+                return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, commentContentRequest.errorMessage());
             }
-            int userId = deleteCommentOnContentRequest.getUserId();
-            if(null == mUserDetailsDao.getUser(userId)){
+            int userId = commentContentRequest.getUserId();
+            if (null == mUserDetailsDao.getUser(userId)) {
                 return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, Constants.INVALID_USER_ID);
             }
-            int contentId = deleteCommentOnContentRequest.getContentId();
-            if(null == mContentDetailsDao.getContentDetails(contentId)){
+            int contentId = commentContentRequest.getContentId();
+            if (null == mContentDetailsDao.getContentDetails(contentId)) {
                 return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, Constants.INVALID_CONTENT_ID);
             }
             boolean status = false;
+            Random rand = new Random();
+            int commentId = rand.nextInt(Integer.MAX_VALUE) + 1;
+            String comment = commentContentRequest.getComment();
             ContentUserCommentAssociation contentUserCommentAssociation = new ContentUserCommentAssociation();
             contentUserCommentAssociation.setUserId(userId);
             contentUserCommentAssociation.setContentId(contentId);
-            contentUserCommentAssociation.setCommentId(deleteCommentOnContentRequest.getCommentId());
-            status = mContentUserCommentDao.deleteContentUserComment(contentUserCommentAssociation);
-
-            if(status == false){
+            contentUserCommentAssociation.setComment(comment);
+            contentUserCommentAssociation.setCommentId(commentId);
+            contentUserCommentAssociation.setIngestionTime(System.currentTimeMillis());
+            status = mContentUserCommentDao.addContentUserComment(contentUserCommentAssociation);
+            if (status == false) {
                 return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, mContentUserCommentDao.getDetailedResponse().getErrorMessage());
-            }else{
-                return ResponseBuilder.successResponse();
+            } else {
+                return ResponseBuilder.successResponse(composeResponse(commentId).toString());
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseBuilder.error(Constants.ERRORCODE_IOEXCEPTION, "Internal Server Error");
         }
+    }
+
+    private JSONObject composeResponse(int commentId) throws JSONException {
+        JSONObject responseObj = new JSONObject();
+        responseObj.put(Constants.SUCCESS, true);
+        responseObj.put(Constants.COMMENT_ID, commentId);
+        return responseObj;
     }
 }
--- src/main/java/com/picturestory/service/api/DeleteComment.java
+++ src/main/java/com/picturestory/service/api/DeleteComment.java
@@ -1,14 +1,13 @@
 package com.picturestory.service.api;
 
-import com.picturestory.service.Constants ;
+
+import com.picturestory.service.Constants;
 import com.picturestory.service.database.dao.IContentDetailsDao;
 import com.picturestory.service.database.dao.IContentUserCommentDao;
 import com.picturestory.service.database.dao.IUserDetailsDao;
 import com.picturestory.service.pojo.ContentUserCommentAssociation;
-import com.picturestory.service.request.AddCommentOnContentRequest;
+import com.picturestory.service.request.DeleteCommentOnContentRequest;
 import com.picturestory.service.response.ResponseBuilder;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -16,69 +15,57 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.util.Random;
+
 /**
  * Created by aasha.medhi on 9/25/15.
  */
-@Path("/addCommentOnContent")
+@Path("/deleteComment")
 @Produces("application/json")
 @Consumes("application/json")
-public class AddCommentContent {
+public class DeleteComment {
     private final IUserDetailsDao mUserDetailsDao;
     private final IContentUserCommentDao mContentUserCommentDao;
     private final IContentDetailsDao mContentDetailsDao;
-
     @Inject
-    public AddCommentContent(IUserDetailsDao userDetailsDao, IContentUserCommentDao contentUserCommentDao, IContentDetailsDao contentDetailsDao) {
+    public DeleteComment(IUserDetailsDao userDetailsDao, IContentUserCommentDao contentUserCommentDao, IContentDetailsDao contentDetailsDao) {
         mUserDetailsDao = userDetailsDao;
         mContentUserCommentDao = contentUserCommentDao;
         mContentDetailsDao = contentDetailsDao;
     }
 
     @POST
-    public Response addCommentOnContent(AddCommentOnContentRequest commentContentRequest) {
+    public Response deleteCommentOnContent(DeleteCommentOnContentRequest deleteCommentOnContentRequest) {
         try {
-
-            if (commentContentRequest == null) {
+            if(deleteCommentOnContentRequest == null){
                 return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, Constants.INVALID_REQUEST);
             }
-            if (!commentContentRequest.isValid()) {
-                return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, commentContentRequest.errorMessage());
+            if(!deleteCommentOnContentRequest.isValid())
+            {
+                return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, deleteCommentOnContentRequest.errorMessage());
             }
-            int userId = commentContentRequest.getUserId();
-            if (null == mUserDetailsDao.getUser(userId)) {
+            int userId = deleteCommentOnContentRequest.getUserId();
+            if(null == mUserDetailsDao.getUser(userId)){
                 return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, Constants.INVALID_USER_ID);
             }
-            int contentId = commentContentRequest.getContentId();
-            if (null == mContentDetailsDao.getContentDetails(contentId)) {
+            int contentId = deleteCommentOnContentRequest.getContentId();
+            if(null == mContentDetailsDao.getContentDetails(contentId)){
                 return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, Constants.INVALID_CONTENT_ID);
             }
             boolean status = false;
-            Random rand = new Random();
-            int commentId = rand.nextInt(Integer.MAX_VALUE) + 1;
-            String comment = commentContentRequest.getComment();
             ContentUserCommentAssociation contentUserCommentAssociation = new ContentUserCommentAssociation();
             contentUserCommentAssociation.setUserId(userId);
             contentUserCommentAssociation.setContentId(contentId);
-            contentUserCommentAssociation.setComment(comment);
-            contentUserCommentAssociation.setCommentId(commentId);
-            contentUserCommentAssociation.setIngestionTime(System.currentTimeMillis());
-            status = mContentUserCommentDao.addContentUserComment(contentUserCommentAssociation);
-            if (status == false) {
+            contentUserCommentAssociation.setCommentId(deleteCommentOnContentRequest.getCommentId());
+            status = mContentUserCommentDao.deleteContentUserComment(contentUserCommentAssociation);
+
+            if(status == false){
                 return ResponseBuilder.error(Constants.ERRORCODE_INVALID_INPUT, mContentUserCommentDao.getDetailedResponse().getErrorMessage());
-            } else {
-                return ResponseBuilder.successResponse(composeResponse(commentId).toString());
+            }else{
+                return ResponseBuilder.successResponse();
             }
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return ResponseBuilder.error(Constants.ERRORCODE_IOEXCEPTION, "Internal Server Error");
         }
-    }
-
-    private JSONObject composeResponse(int commentId) throws JSONException {
-        JSONObject responseObj = new JSONObject();
-        responseObj.put(Constants.SUCCESS, true);
-        responseObj.put(Constants.COMMENT_ID, commentId);
-        return responseObj;
     }
 }
