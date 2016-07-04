1fce2e866433e8f310a583baf5ac167c1fa24e96
Prasanna Bankuru <bankuru.prasanna@inmobi.com>
2016-07-04 23:01:16 +0530

    updated endpoint

--- src/main/java/com/picturestory/service/Main.java
+++ src/main/java/com/picturestory/service/Main.java
@@ -21,7 +21,7 @@ import java.net.URI;
  */
 public class Main {
     private HttpServer server;
-    public static final String BASE_URI = "http://localhost:4055/";
+    public static final String BASE_URI = "http://ec2-52-207-219-87.compute-1.amazonaws.com:4055/";
 
     public static HttpServer startServer(){
         try {
