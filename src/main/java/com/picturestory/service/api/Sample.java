6482fd9bbe26285c187ef0bd678e9fd427f61f13
Prasanna Bankuru <bankuru.prasanna@inmobi.com>
2016-07-06 15:19:03 +0530

    updated config

--- src/main/java/com/picturestory/service/api/Sample.java
+++ src/main/java/com/picturestory/service/api/Sample.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.awt.*;
 
-@Path("myResource")
+@Path("ping")
 @Produces(MediaType.APPLICATION_JSON)
 public class Sample {
     @GET
